237ef8f2da169c7f787f2da93e2729fd2c98f13c
Jacob Langley <jacob.langley@ghtns.com>
2022-12-01 06:44:10 -0800

    day1, part 1

--- day01-1/src/main.rs
+++ day01-1/src/main.rs
@@ -1,3 +1,45 @@
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+
+pub fn file_to_lines(path: &String) -> Vec<String> {
+    let path = Path::new(path);
+
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", path.display(), why),
+        Ok(file) => file,
+    };
+
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", path.display(), why),
+        Ok(_) => {}
+    };
+
+    let lines : Vec<String> = s.split("\n").collect::<Vec<&str>>().iter().map(|x| x.to_string()).collect::<Vec<String>>();
+    lines
+}
+
+fn part1(path: &String) {
+    println!("File: {path}");
+    let lines = file_to_lines(path);
+
+    let mut elf_calories = vec![];
+
+    for elf in lines.split(|element| element.len() == 0) {
+        let calories = elf.iter().map(|s| s.parse::<i32>().unwrap() ).sum::<i32>();
+        elf_calories.push(calories);
+    }
+
+    let max_calories = elf_calories.iter().fold(std::i32::MIN, |a,b| a.max(*b));
+    println!("{}", max_calories.to_string());
+}
+
 fn main() {
-    println!("Hello, World!");
+    let file = std::env::args().nth(1);
+
+    match file {
+        Some(file) => part1(&file),
+        None => println!("No file"),
+    }
 }
