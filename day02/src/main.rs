752427fe53f3c28cd7ed463490ba5598cb04d558
Jacob Langley <jacob.langley@ghtns.com>
2022-12-02 07:13:56 -0800

    day2, part2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
@@ -68,17 +70,78 @@ fn part1(path: &String) {
     println!("{}", score);
 }
 
+fn score_choice2(c: char) -> i32 {
+    match c {
+        'A' => 1 , // Rock,
+        'B' => 2 , // Paper,
+        'C' => 3 , // Scisors,
+        _ => -1,
+    }
+}
+
+fn translate2(t: char, s: char) -> char {
+    if s == 'X' /* lose */{
+        return match t {
+            'A' => 'C',
+            'B' => 'A',
+            'C' => 'B',
+            _ => '0' // shut up, rust
+        }        
+    }
+    else if s == 'Z' /* win */{
+        return match t {
+            'A' => 'B',
+            'B' => 'C',
+            'C' => 'A',
+            _ => '0' // shut up, rust
+        }        
+    }
+
+    return t
+}
+
+fn score_line2(line: &String) -> i32 {
+    // a draw
+    let them = line.chars().nth(0).unwrap();
+    let strategy = line.chars().nth(2).unwrap();
+    let you = translate2(them, strategy);
+
+    if you == 'A' && them == 'C' {
+        return 6 + score_choice2(you)
+    }
+    else if you == 'A' && them == 'A' {
+        return 3 + score_choice2(you)
+    }
+
+    else if you == 'B' && them == 'A' {
+        return 6 + score_choice2(you)
+    }
+    else if you == 'B' && them == 'B' {
+        return 3 + score_choice2(you)
+    }
+
+    else if you == 'C' && them == 'B' {
+        return 6 + score_choice2(you)
+    }
+    else if you == 'C' && them == 'C' {
+        return 3 + score_choice2(you)
+    }
+
+    return score_choice2(you)
+}
+
 fn part2(path: &String) {
     println!("File: {path}");
     let lines = file_to_lines(path);
-
+    let score = lines.iter().map(|l| score_line2(l)).sum::<i32>();
+    println!("{}", score);
 }
 
 fn main() {
     let file = std::env::args().nth(1);
 
     match file {
-        Some(file) => part1(&file),
+        Some(file) => part2(&file),
         None => println!("No file"),
     }
 }
