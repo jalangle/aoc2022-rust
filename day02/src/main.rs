48e37d1958fd61d7cc15fe58f8ac9ac544dc0874
Jacob Langley <jacob.langley@ghtns.com>
2022-12-02 06:58:52 -0800

    day2 part 1

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -20,43 +20,59 @@ pub fn file_to_lines(path: &String) -> Vec<String> {
     lines
 }
 
+fn score_choice(c: char) -> i32 {
+    match c {
+        'A' => 0 , // Rock,
+        'B' => 0 , // Paper,
+        'C' => 0 , // Scisors,
+        'X' => 1 , // Rock,
+        'Y' => 2 , // Paper,
+        'Z' => 3 , // Scissors,
+        _ => -1,
+    }
+}
+
+fn score_line(line: &String) -> i32 {
+    // a draw
+    let them = line.chars().nth(0).unwrap();
+    let you = line.chars().nth(2).unwrap();
+
+    if you == 'X' && them == 'C' {
+        return 6 + score_choice(you)
+    }
+    else if you == 'X' && them == 'A' {
+        return 3 + score_choice(you)
+    }
+
+    else if you == 'Y' && them == 'A' {
+        return 6 + score_choice(you)
+    }
+    else if you == 'Y' && them == 'B' {
+        return 3 + score_choice(you)
+    }
+
+    else if you == 'Z' && them == 'B' {
+        return 6 + score_choice(you)
+    }
+    else if you == 'Z' && them == 'C' {
+        return 3 + score_choice(you)
+    }
+
+    return score_choice(you)
+}
+
 fn part1(path: &String) {
     println!("File: {path}");
     let lines = file_to_lines(path);
-
-    let mut elf_calories = vec![];
-
-    for elf in lines.split(|element| element.len() == 0) {
-        let calories = elf.iter().map(|s| s.parse::<i32>().unwrap() ).sum::<i32>();
-        elf_calories.push(calories);
-    }
-
-    let max_calories = elf_calories.iter().fold(std::i32::MIN, |a,b| a.max(*b));
-    println!("{}", max_calories.to_string());
-}
-
-fn part2(path: &String) {
-    println!("File: {path}");
-    let lines = file_to_lines(path);
-
-    let mut elf_calories = vec![];
-
-    for elf in lines.split(|element| element.len() == 0) {
-        let calories = elf.iter().map(|s| s.parse::<i32>().unwrap() ).sum::<i32>();
-        elf_calories.push(calories);
-    }
-
-    elf_calories.sort();
-    elf_calories.reverse();
-    let top_three = elf_calories[0..3].iter().sum::<i32>();
-    println!("{}", top_three.to_string());
+    let score = lines.iter().map(|l| score_line(l)).sum::<i32>();
+    println!("{}", score);
 }
 
 fn main() {
     let file = std::env::args().nth(1);
 
     match file {
-        Some(file) => part2(&file),
+        Some(file) => part1(&file),
         None => println!("No file"),
     }
 }
