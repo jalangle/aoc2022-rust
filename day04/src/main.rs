d9acbd71c162c5736a78c1e4c1f636b5bef3ae08
Jacob Langley <jacob.langley@ghtns.com>
2022-12-04 08:58:13 -0800

    day4, part1

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -5,6 +5,7 @@
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
+use std::collections::HashSet;
 
 pub fn file_to_lines(path: &String) -> Vec<String> {
     let path = Path::new(path);
@@ -24,10 +25,31 @@ pub fn file_to_lines(path: &String) -> Vec<String> {
     lines
 }
 
+fn set_from_string(range: &String) -> HashSet<i32> {
+    let parts : Vec<i32> = range.split("-").collect::<Vec<&str>>().iter().map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    let mut set : HashSet<i32>  = HashSet::new();
+    for i in parts[0]..parts[1]+1 {
+        set.insert(i);
+    }
+    return set 
+}
 
 fn part1(path: &String) {
     println!("File: {path}");
     let lines = file_to_lines(path);
+    let mut count : i32 = 0;
+    for l in lines {
+        let elves = l.split(",").collect::<Vec<&str>>().iter().map(|x| x.to_string()).collect::<Vec<String>>();
+        let first_range = set_from_string(&elves[0]);
+        let second_range = set_from_string(&elves[1]);
+
+        let intersection = first_range.intersection(&second_range).collect::<Vec<&i32>>();
+        if first_range.len() == intersection.len() || second_range.len() == intersection.len() {
+            count += 1;
+        }
+    }
+
+    println!("{}", count);
 }
 
 fn part2(path: &String) {
