e6fcc64d3167dcf142e46dcee1ee7d2158e52453
Jacob Langley <jacob.langley@ghtns.com>
2022-12-05 07:20:31 -0800

    Day5, part 1

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -5,7 +5,6 @@
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
-use std::collections::HashSet;
 
 pub fn file_to_lines(path: &String) -> Vec<String> {
     let path = Path::new(path);
@@ -25,10 +24,66 @@ pub fn file_to_lines(path: &String) -> Vec<String> {
     lines
 }
 
+fn remove_whitespace(s: &mut String) {
+    s.retain(|c| !c.is_whitespace() );
+}
 
 fn part1(path: &String) {
     println!("File: {path}");
     let lines = file_to_lines(path);
+
+    let sep = lines.iter().position(|s| s == "" ).unwrap();
+    let buckets = &lines[0..sep-1];
+    let instructions = &lines[sep+1..];
+
+    let mut stacks : Vec<Vec<String>> = vec![];
+    let count_of_buckets = buckets[0].chars().collect::<Vec<char>>().chunks(4).count();
+
+    for i in 0..count_of_buckets {
+        stacks.push(Vec::<String>::new());
+    }
+
+    for x in buckets {
+        let mut index : usize = 0;
+
+        for y in x.chars().collect::<Vec<char>>().chunks(4) {
+            let mut s = String::from_iter(y);
+            remove_whitespace(&mut s);
+            if s != "" {
+                stacks[index].insert(0, s);
+            }
+            index+=1;
+        }
+    }
+    /*
+    for stack in stacks {
+        println!("{}", stack.join(" "));
+        println!("-----");
+    }
+    */
+
+    for i in instructions {
+        println!("-----");
+        println!("{}", i);
+        let parts = i.split(" ").collect::<Vec<&str>>();
+        let count = parts[1].parse::<usize>().unwrap();
+        let from_bucket = parts[3].parse::<usize>().unwrap() - 1;
+        let to_bucket = parts[5].parse::<usize>().unwrap() - 1;
+
+        for c in 0..count {
+            let e = stacks[from_bucket].pop().unwrap();
+            stacks[to_bucket].push(e);
+        }
+
+        for stack in &stacks {
+            println!("{}", stack.join(" "));
+        }
+    }
+
+    println!("=====");
+    for mut stack in stacks {
+        println!("{}", stack.pop().unwrap());
+    }
 }
 
 fn part2(path: &String) {
