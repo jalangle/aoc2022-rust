9efc90c6421f26ef67b79c2ddce1b0e14882cd0d
Jacob Langley <jacob.langley@ghtns.com>
2022-12-06 07:35:12 -0800

    Add day6 solutions

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -24,22 +24,56 @@ pub fn file_to_lines(path: &String) -> Vec<String> {
     lines
 }
 
+fn all_unique(chars : Vec<char>) -> bool {
+    let mut has : Vec<char> = vec![];
+    for c in chars {
+        if has.contains(&c) {
+            return false;
+        }
+        has.push(c);
+    }
+    return true;
+}
+
 fn part1(path: &String) {
     println!("File: {path}");
     let lines = file_to_lines(path);
 
+    for line in lines {
+        let chars: Vec<char> = line.chars().collect::<Vec<_>>();
+        let mut counter = 4;
+        for window in chars.windows(counter) {
+            if all_unique(window.to_vec()) {
+                break;
+            }
+            counter += 1;
+        }
+        println!("{} {}", line, counter);
+    }
 }
 
 fn part2(path: &String) {
     println!("File: {path}");
     let lines = file_to_lines(path);
+
+    for line in lines {
+        let chars: Vec<char> = line.chars().collect::<Vec<_>>();
+        let mut counter = 14;
+        for window in chars.windows(counter) {
+            if all_unique(window.to_vec()) {
+                break;
+            }
+            counter += 1;
+        }
+        println!("{} {}", line, counter);
+    }
 }
 
 fn main() {
     let file = std::env::args().nth(1);
 
     match file {
-        Some(file) => part1(&file),
+        Some(file) => part2(&file),
         None => println!("No file"),
     }
 }
