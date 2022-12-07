67b2a27f391035b0b8e7d429918898389690bd1d
Jacob Langley <jacob.langley@ghtns.com>
2022-12-07 07:35:43 -0800

    Day7, part1

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -20,13 +20,78 @@ pub fn file_to_lines(path: &String) -> Vec<String> {
         Ok(_) => {}
     };
 
-    let lines : Vec<String> = s.split("\n").collect::<Vec<&str>>().iter().map(|x| x.to_string()).collect::<Vec<String>>();
+    let lines : Vec<String> = s.split("\n").collect::<Vec<&str>>().iter().map(|x| x.to_string()).collect();
     lines
 }
 
 fn part1(path: &String) {
     println!("File: {path}");
     let lines = file_to_lines(path);
+
+    let mut cwd : String = "".to_string();
+    let mut current_command : String = "".to_string();
+
+    let mut dirs : Vec<String> = vec![];
+    let mut sizes : Vec<String> = vec![];
+
+    for l in lines {
+        if l.starts_with("$") {
+            let command_parts : Vec<String> = l.split(' ').map(|s| s.to_string()).collect();
+            current_command = command_parts[1].clone();
+            if command_parts[1] == "cd" {
+                if command_parts[2] == "/" {
+                    cwd = "".to_string();
+                }
+                else if command_parts[2] == ".." {
+                    let mut path_parts : Vec<String> = cwd.split('/').map(|s| s.to_string()).collect();
+                    path_parts.pop();
+                    cwd = path_parts.join("/");
+                }
+                else {
+                    cwd += "/";
+                    cwd += &command_parts[2];
+
+                    if cwd != "" && !dirs.contains(&cwd) {
+                        dirs.push(cwd.clone());
+                    }
+                }
+                //println!("{} >", cwd);
+            }
+            else if command_parts[1] == "ls" {
+                //println!("ls");
+            }
+        }
+        else {
+            if current_command == "ls" {
+                if l.starts_with("dir") {
+
+                }
+                else {
+                    let line_parts : Vec<String> = l.split(' ').map(|s| s.to_string()).collect();
+                    let size : usize = line_parts[0].parse::<usize>().unwrap();
+                    let name = line_parts[1].to_string();
+                    //println!("{} {}", size, name);
+                    sizes.push(format!("{}-{}-{}", cwd, name, size));
+                }
+            }
+
+        }
+    }
+
+    let mut total_size : usize = 0;
+    for d in dirs {
+        let mut objects_in_dir : Vec<String> = sizes.clone();
+        objects_in_dir.retain(|s| s.starts_with(&d));
+        let mut size : usize = 0;
+        for o in objects_in_dir {
+            let line_parts : Vec<String> = o.split('-').map(|s| s.to_string()).collect();
+            size += line_parts[2].parse::<usize>().unwrap();
+        }
+        if size <= 100000 {
+            total_size += size;
+        }
+    }
+    println!("{}", total_size.to_string()); // 1331842 too low
 }
 
 fn part2(path: &String) {
