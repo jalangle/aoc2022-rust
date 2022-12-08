902ad195fe25afcbb7c162cac3da57808e32152a
Jacob Langley <jacob.langley@ghtns.com>
2022-12-08 06:14:59 -0800

    day8, part1

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -24,9 +24,100 @@ pub fn file_to_lines(path: &String) -> Vec<String> {
     lines
 }
 
+struct Tree {
+    height: usize,
+    visible: bool,
+}
+
+fn lines_to_grid(lines: &Vec<String>) -> Vec<Vec<Tree>> {
+    let mut grid : Vec<Vec<Tree>> = vec![];
+    for l in lines {
+        let tree_row : Vec<Tree> = l.chars()
+                                    .map(|c| return Tree{
+                                        height: c.to_string().parse::<usize>().unwrap(),
+                                        visible:false })
+                                    .collect();
+        grid.push(tree_row);
+    }
+
+    return grid
+}
+
+fn mark_visible(mut grid : &mut Vec<Vec<Tree>>) {
+
+    let size : usize = grid[0].len();
+
+    /* mark edges */
+    for i in 0..size {
+        grid[0][i].visible = true;
+        grid[size-1][i].visible = true;
+    }
+    for i in 0..size {
+        grid[i][0].visible = true;
+        grid[i][size-1].visible = true;
+    }
+
+    for x in 1..size-1 {
+        /* shoot a beam from the left marking anything we can see */
+        let mut max : usize = grid[x][0].height;
+        for y in 1..size-1 {
+            if grid[x][y].height > max {
+                grid[x][y].visible = true;
+                max = grid[x][y].height;         
+            }
+        }
+
+        /* shoot a beam from the right marking anything we can see */
+        max = grid[x][size-1].height;
+        for y in (1..size-1).rev() {
+            if grid[x][y].height > max {
+                grid[x][y].visible = true;
+                max = grid[x][y].height;
+            }
+        }
+    }
+
+    for y in 1..size-1 {
+        /* shoot a beam from the top marking anything we can see */
+        let mut max : usize = grid[0][y].height;
+        for x in 1..size-1 {
+            if grid[x][y].height > max {
+                grid[x][y].visible = true;
+                max = grid[x][y].height;
+            }
+        }
+
+        /* shoot a beam from the bottom marking anything we can see */
+        max = grid[size-1][y].height;
+        for x in (1..size-1).rev() {
+            if grid[x][y].height > max {
+                grid[x][y].visible = true;
+                max = grid[x][y].height;
+            }
+        }
+    }
+}
+
 fn part1(path: &String) {
     println!("File: {path}");
     let lines = file_to_lines(path);
+    let mut grid = lines_to_grid(&lines);
+    mark_visible(&mut grid);
+
+    let mut visible : usize = 0;
+    for row in grid {
+        for col in row {
+            if col.visible {
+                visible += 1;
+                print!("o");
+            }
+            else {
+                print!("X");
+            }
+        }
+        println!("");
+    }
+    println!("{}", visible);
 }
 
 fn part2(path: &String) {
