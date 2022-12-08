6aaa698f3a4e917edb67bca6f2e43c440cfc1bb6
Jacob Langley <jacob.langley@ghtns.com>
2022-12-08 07:24:11 -0800

    day8, part2

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -43,7 +43,7 @@ fn lines_to_grid(lines: &Vec<String>) -> Vec<Vec<Tree>> {
     return grid
 }
 
-fn mark_visible(mut grid : &mut Vec<Vec<Tree>>) {
+fn shoot_lasers(mut grid : &mut Vec<Vec<Tree>>) {
 
     let size : usize = grid[0].len();
 
@@ -102,7 +102,7 @@ fn part1(path: &String) {
     println!("File: {path}");
     let lines = file_to_lines(path);
     let mut grid = lines_to_grid(&lines);
-    mark_visible(&mut grid);
+    shoot_lasers(&mut grid);
 
     let mut visible : usize = 0;
     for row in grid {
@@ -120,16 +120,75 @@ fn part1(path: &String) {
     println!("{}", visible);
 }
 
+fn climb_trees(mut grid : &mut Vec<Vec<Tree>>) -> usize {
+    let size : usize = grid[0].len();
+
+    let mut best_score : usize = 0;
+
+    /* climb each center tree */
+    for row in 0..size {
+        for col in 0..size {
+            //println!("-----------");
+            //println!("({},{})", row, col);
+            let height = grid[row][col].height;
+
+            let mut left_distance : usize = 0;
+            let mut right_distance : usize = 0;
+            let mut up_distance : usize = 0;
+            let mut down_distance : usize = 0;
+
+            for left in (0..col).rev() {
+                left_distance += 1;
+                if height <= grid[row][left].height {
+                    break;
+                }
+            }
+
+            for right in col+1..size {
+                right_distance += 1;
+                if height <= grid[row][right].height {
+                    break;
+                }
+            }
+
+            for up in (0..row).rev() {
+                up_distance += 1;
+                if height <= grid[up][col].height {
+                    break;
+                }
+            }
+
+            for down in row+1..size {
+                down_distance += 1;
+                if height <= grid[down][col].height {
+                    break;
+                }
+            }
+
+            let score = left_distance * right_distance * up_distance * down_distance;
+            //println!("{} {} | {} {} {} {} => {}", row, col, left_distance, right_distance, up_distance, down_distance, score.to_string());
+            if score > best_score {
+                best_score = score;
+            }
+        }
+    }
+    return best_score
+}
+
 fn part2(path: &String) {
+
     println!("File: {path}");
     let lines = file_to_lines(path);
+    let mut grid = lines_to_grid(&lines);
+    let best_score = climb_trees(&mut grid);
+    println!("best: {}", best_score.to_string())
 }
 
 fn main() {
     let file = std::env::args().nth(1);
 
     match file {
-        Some(file) => part1(&file),
+        Some(file) => part2(&file),
         None => println!("No file"),
     }
 }
